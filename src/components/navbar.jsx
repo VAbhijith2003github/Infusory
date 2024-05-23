af3a5ca872d553e16fcbb81087c6b5a384d419e5
V Abhijith <119759593+VAbhijith2003github@users.noreply.github.com>
2024-05-23 23:58:43 +0530

    adds navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <h1>HOME</h1>
+        <h1>DRIVE</h1>
+        <h1>ABOUT</h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
